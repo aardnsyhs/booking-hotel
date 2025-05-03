fb5955ab3f477721e751b1d11a03adc53525d0a8
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-03 15:32:58 +0700

    feat: integrate NextAuth for session management and role-based access control

--- components/navbar/NavLink.tsx
+++ components/navbar/NavLink.tsx
@@ -3,12 +3,36 @@ import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 import clsx from "clsx";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const NavLink = () => {
   const [open, setOpen] = useState(false);
+  const { data: session } = useSession();
 
   return (
     <>
+      {session?.user ? (
+        <div className="flex items-center justify-end md:order-2">
+          <div className="hidden text-sm bg-gray-50 border rounded-full md:me-0 md:block focus:ring-4 focus:ring-gray-300">
+            <Image
+              src={session.user.image || "/img/avatar.svg"}
+              alt={session.user.name || "Avatar"}
+              width={64}
+              height={64}
+              className="size-8 rounded-full"
+            />
+          </div>
+          <div className="flex items-center">
+            <button
+              onClick={() => signOut()}
+              className="md:block hidden py-2 px-4 bg-gray-50 text-gray-700 hover:bg-gray-100 rounded-sm cursor-pointer"
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      ) : null}
       <button
         onClick={() => setOpen(!open)}
         className="inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100"
@@ -53,38 +77,57 @@ const NavLink = () => {
               Contact
             </Link>
           </li>
-          <li>
-            <Link
-              href="/reservations"
-              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
-            >
-              My Reservations
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/admin/dashboard"
-              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
-            >
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/admin/room"
-              className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
-            >
-              Manage Rooms
-            </Link>
-          </li>
-          <li className="pt-2 md:pt-0">
-            <Link
-              href="/signin"
-              className="py-2.5 px-6 bg-orange-400 text-white hover:bg-orange-500 rounded-sm"
-            >
-              Sign In
-            </Link>
-          </li>
+          {session && (
+            <>
+              <li>
+                <Link
+                  href="/reservations"
+                  className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                >
+                  My Reservations
+                </Link>
+              </li>
+              {session.user.role === "admin" && (
+                <>
+                  <li>
+                    <Link
+                      href="/admin/dashboard"
+                      className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/admin/room"
+                      className="block py-2 px-3 text-gray-800 hover:bg-gray-100 rounded-sm md:hover:bg-transparent md:p-0"
+                    >
+                      Manage Rooms
+                    </Link>
+                  </li>
+                </>
+              )}
+            </>
+          )}
+          {session ? (
+            <li className="pt-2 md:pt-0">
+              <button
+                onClick={() => signOut()}
+                className="py-2.5 px-4 bg-red-400 text-white hover:bg-red-600 rounded-sm cursor-pointer md:hidden"
+              >
+                Sign Out
+              </button>
+            </li>
+          ) : (
+            <li className="pt-2 md:pt-0">
+              <Link
+                href="/signin"
+                className="py-2.5 px-6 bg-orange-400 text-white hover:bg-orange-500 rounded-sm"
+              >
+                Sign In
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </>
---
+++ types/authjs.d.ts
@@ -0,0 +1,19 @@
+import { type DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface User {
+    role: string;
+  }
+
+  interface Session {
+    user: User & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    sub: string;
+    role: string;
+  }
+}
