6e687587e7c23c3026d04da8c1c2cebcccad5307
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-03 13:07:30 +0700

    feat: add Main and Card components for room listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Room & Rates</h1>
+          <p className="py-3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequuntur, consectetur.
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   );
 }
