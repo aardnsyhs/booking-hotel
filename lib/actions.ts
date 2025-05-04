505b583697801d7157eadfab0cab078348a0c103
Ardiansyah Sulistyo <ardiansyahdian755@gmail.com>
2025-05-04 11:54:47 +0700

    feat: add contact page and form; implement server-side handling and validation for contact messages

---
+++ lib/actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { prisma } from "./prisma";
+import { ContactSchema } from "./zod";
+
+export const ContactMessage = async (
+  prevState: unknown,
+  formData: FormData
+) => {
+  const validatedFields = ContactSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!validatedFields.success) {
+    return { error: validatedFields.error.flatten().fieldErrors };
+  }
+
+  const { name, email, subject, message } = validatedFields.data;
+
+  try {
+    await prisma.contact.create({
+      data: {
+        name,
+        email,
+        subject,
+        message,
+      },
+    });
+    return { message: "Thanks for contact us." };
+  } catch (err) {
+    console.error(err);
+  }
+};
